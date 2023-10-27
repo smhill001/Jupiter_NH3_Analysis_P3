975e68d32d5f74617c5e51644a69bc99eec3d75e
Steven Hill <smhill001@gmail.com>
2023-10-26 20:42:53 -0600

    Pre-trip commit - Lots of updated data
    Really ran through 2022 and much of 2023 to produce FITS files etc.

--- WINJupos_CM.bat
+++ WINJupos_CM.bat
@@ -1,2 +1,2 @@
 cd "\Program Files\WinJUPOS 12.1.1"
-WinJUPOS.x64.exe Jupiter /GetCM:2023-09-06_12:03:12 /GeoLong:-104.9 /GeoLat:39.7 /GetAlt >"c:\Astronomy\Projects\SAS 2021 Ammonia\Jupiter_NH3_Analysis_P3\cm.txt"
+WinJUPOS.x64.exe Jupiter /GetCM:2023-10-15_05:22:36 /GeoLong:-104.9 /GeoLat:39.7 /GetAlt >"c:\Astronomy\Projects\SAS 2021 Ammonia\Jupiter_NH3_Analysis_P3\cm.txt"
